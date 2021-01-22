921f0bbafe62671813765c36a49c0bc462f44458
Sicheng Wei <62972197+Sicheng-Wei@users.noreply.github.com>
2021-01-22 10:49:42 +0800

    Update VH_integration.m

--- Boids with Hunter/Vicsek/VH_integration.m
+++ Boids with Hunter/Vicsek/VH_integration.m
@@ -5,10 +5,17 @@ function [] = V_integrate(T,N,R,v0,tinf,tsup)
 %参数说明：
 %N - 集群数目；R - 鱼群个体影响半径；v0 - 基础速度；tinf,tsup - 噪声范围
     %% 各参数初始化
-    x = -10 + 30 * rand(N,1);
-    y = -5 + 20 * rand(N,1);
-    angle = 2 * pi * rand(N,1);
-    velo = zeros(N,1);
+    BOIDS.x = -50 + 70 * rand(N,1);
+    BOIDS.y = -25 + 40 * rand(N,1);
+    BOIDS.angle = 2 * pi * rand(N,1);
+    BOIDS.velo = zeros(N,1);
+    
+    HUNTER.x = zeros(1,1);
+    HUNTER.y = zeros(1,1);
+    HUNTER.angle = zeros(1,1);
+    HUNTER.velo = zeros(1,1);
+    HUNTER = Hunter_Trace(HUNTER.x);
+    
     for i = 1:N
         BOIDS.velo(i) = v0;
     end
@@ -16,32 +23,22 @@ function [] = V_integrate(T,N,R,v0,tinf,tsup)
     %% 操作函数集
     for i = 1:T
         hold on
-        Vplot(x,y,angle,i);
-        [x,y,angle,velo] = Hunter_Threat(x,y,angle,velo,i);
-        [x,y,angle,velo] = Basic_Vicsek(N,R,tinf,tsup,x,y,angle,velo);
-        %疲倦参数
-        indexFast = find(velo > v0);
-        velo(indexFast) = velo(indexFast) - 0.02;
+        Vplot(BOIDS,HUNTER,i);
+        HUNTER = Hunter_Trace(i);
+        BOIDS = Hunter_Threat(BOIDS,HUNTER);
+        BOIDS = Basic_Vicsek(N,R,tinf,tsup,BOIDS);
+        %速度回归参数
+        indexFast = find(BOIDS.velo > v0);
+        BOIDS.velo(indexFast) = BOIDS.velo(indexFast) - 0.03;
         hold off
         clf
     end
 end
 
 %% 绘图
-function[] = Vplot(x,y,angle,times)
-    quiver(x,y,0.3 * cos(angle),0.3 * sin(angle),'AutoScale','off');
-    xh = -10 + 0.2 * times; yh = 5;
-    quiver(xh,yh,1,0);
-    
-    %邻接区
-    theta = linspace(0,2 * pi,10000);
-    C1 = xh + 3 * cos(theta); C2 = yh + 3 * sin(theta);
-    plot(C1,C2,'g');
-    %视野区
-    alpha = linspace(-pi/2,pi/2,1000);
-    C3 = xh + 6 * cos(alpha); C4 = yh + 6 * sin(alpha);
-    plot(C3,C4,'g');
-    
+function[] = Vplot(BOIDS,HUNTER,times)
+    quiver(BOIDS.x,BOIDS.y,0.3 * cos(BOIDS.angle),0.3 * sin(BOIDS.angle),'AutoScale','off');
+    quiver(HUNTER.x,HUNTER.y,cos(HUNTER.angle),sin(HUNTER.angle),'AutoScale','off');
     axis([-10,20,-5,15]);
     axis equal
     title(['Times = ',num2str(times)]);
@@ -52,7 +49,7 @@ function[] = Vplot(x,y,angle,times)
     end
 end
 
-%% Hunter�켣
+%% Hunter轨迹
 function [HUNTER] = Hunter_Trace(time)
     HUNTER.x = -60 + 0.2 * time;
     HUNTER.y = 5;
@@ -60,10 +57,10 @@ function [HUNTER] = Hunter_Trace(time)
 end
 
 %% Vicsek基础模型
-function [x,y,angle,velo] = Basic_Vicsek(N,R,tinf,tsup,x,y,angle,velo)
-    x = x + velo .* cos(angle);
-    y = y + velo .* sin(angle);
-    regangle = angle; regvelo = velo;
+function [BOIDS] = Basic_Vicsek(N,R,tinf,tsup,BOIDS)
+    BOIDS.x = BOIDS.x + BOIDS.velo .* cos(BOIDS.angle);
+    BOIDS.y = BOIDS.y + BOIDS.velo .* sin(BOIDS.angle);
+    regangle = BOIDS.angle; regvelo = BOIDS.velo;
     for i = 1:N
         unit = sqrt((BOIDS.x - BOIDS.x(i)).^2 + (BOIDS.y - BOIDS.y(i)).^2);
         index = find(unit <= R^2);
@@ -75,21 +72,21 @@ function [x,y,angle,velo] = Basic_Vicsek(N,R,tinf,tsup,x,y,angle,velo)
 end
 
 %% Vicsek捕食者项修正
-function[x,y,angle,velo] = Hunter_Threat(x,y,angle,velo,times)
-    xh = -10 + 0.2 * times; yh = 5; escv = 0.03;
-    dangle = atan2(y - yh,x - xh);
-    adjacent = sqrt((x - xh).^2 + (y - yh).^2);
+function[BOIDS] = Hunter_Threat(BOIDS,HUNTER)
+    escv = 0.20;
+    dangle = atan2(BOIDS.y - HUNTER.y,BOIDS.x - HUNTER.x) - HUNTER.angle;
+    adjacent = sqrt((BOIDS.y - HUNTER.y).^2 + (BOIDS.x - HUNTER.x).^2);
     %邻接区修正
     indexAD = find(adjacent <= 3);
-    angle(indexAD) = dangle(indexAD);
-    velo(indexAD) = (3 - adjacent(indexAD)) * 0.13 + 0.07;
+    BOIDS.angle(indexAD) = dangle(indexAD);
+    BOIDS.velo(indexAD) = (3 - adjacent(indexAD)) * 0.30 + 0.08;
     %视野区修正
-    inVSp = find(adjacent <= 6 & dangle < pi / 2 & dangle > 0);
-    vxp = velo(inVSp).* cos(angle(inVSp)) + escv * cos(dangle(inVSp)).* cos(dangle(inVSp) + pi / 2);
-    vyp = velo(inVSp).* sin(angle(inVSp)) + escv * cos(dangle(inVSp)).* sin(dangle(inVSp) + pi / 2);
-    angle(inVSp) = atan2(vyp,vxp); velo(inVSp) = sqrt(vxp.^2 + vyp.^2);
-    inVSn = find(adjacent <= 6 & dangle < 0 & dangle >-pi / 2);
-    vxn = velo(inVSn).* cos(angle(inVSn)) + escv * cos(dangle(inVSn)).* cos(dangle(inVSn) - pi / 2);
-    vyn = velo(inVSn).* sin(angle(inVSn)) + escv * cos(dangle(inVSn)).* sin(dangle(inVSn) - pi / 2);
-    angle(inVSn) = atan2(vyn,vxn); velo(inVSn) = sqrt(vxn.^2 + vyn.^2);
+    inVSp = find(adjacent <= 8 & dangle < pi / 2 & dangle > 0);
+    vxp = BOIDS.velo(inVSp).* cos(BOIDS.angle(inVSp)) + escv * cos(dangle(inVSp)).* cos(dangle(inVSp) + pi / 2);
+    vyp = BOIDS.velo(inVSp).* sin(BOIDS.angle(inVSp)) + escv * cos(dangle(inVSp)).* sin(dangle(inVSp) + pi / 2);
+    BOIDS.angle(inVSp) = atan2(vyp,vxp); BOIDS.velo(inVSp) = sqrt(vxp.^2 + vyp.^2);
+    inVSn = find(adjacent <= 8 & dangle < 0 & dangle >-pi / 2);
+    vxn = BOIDS.velo(inVSn).* cos(BOIDS.angle(inVSn)) + escv * cos(dangle(inVSn)).* cos(dangle(inVSn) - pi / 2);
+    vyn = BOIDS.velo(inVSn).* sin(BOIDS.angle(inVSn)) + escv * cos(dangle(inVSn)).* sin(dangle(inVSn) - pi / 2);
+    BOIDS.angle(inVSn) = atan2(vyn,vxn); BOIDS.velo(inVSn) = sqrt(vxn.^2 + vyn.^2);
 end
